e5dcfa9afd5aa09f8a7117fa95be416713405a7f
Jenni Hautala <jenni.hautala@viidakko.fi>
2022-01-19 16:28:17 +0200

    Drag handler

--- src/components/Player.js
+++ src/components/Player.js
@@ -8,9 +8,12 @@ import React, { useEffect, useRef, useState } from "react";
 const Player = ({ isPlaying, setIsPlaying, currentSong }) => {
 	// Ref
 	const audioRef = useRef(null);
+
+	// State
 	const [songInfo, setSongInfo] = useState({
-		currentTime: null,
-		duration: null,
+		// Huom! Nää ei saa olla null
+		currentTime: 0,
+		duration: 0,
 	})
 
 	// Event Handlers
@@ -23,7 +26,7 @@ const Player = ({ isPlaying, setIsPlaying, currentSong }) => {
 		setIsPlaying(!isPlaying);
 	}
 	const timeUpdateHandler = (e) => {
-		console.log(e)
+		// console.log(e)
 		const current = e.target.currentTime;
 		const duration = e.target.duration;
 		// Setting song info as an object
@@ -37,21 +40,38 @@ const Player = ({ isPlaying, setIsPlaying, currentSong }) => {
 	 * For formatting time nicely
 	 * @param {*} time
 	 */
-	const getTime = (time) => 
+	const getTime = (time) =>
 			`${Math.floor(time / 60)}:${('0' + Math.floor(time % 60)).slice(-2)}`
-		
-	// State
+	const dragHandler = (e) => {
+		// Päitetään audiokohta siihen mihin draggaillaan
+		audioRef.current.currentTime = e.target.value;
+
+		// Rangen eri arvot kun draggaillaan: e.target.value
+		// Päivitetään songInfo-objektin currentTime siihen mihin draggaillaan
+		setSongInfo({...songInfo, currentTime: e.target.value});
+	}
 
 	return(
 		<div className="player">
 			<div className="time-control">
 				<p>{getTime(songInfo.currentTime)}</p>
-				<input type="range" value={songInfo.currentTime} />
+				<input
+					min={0}
+					max={songInfo.duration}
+					type="range"
+					value={songInfo.currentTime}
+					onChange={dragHandler}
+				/>
 				<p>{getTime(songInfo.duration)}</p>
 			</div>
 			<div className="play-control">
 				<FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft}/>
-				<FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={isPlaying ? faPause : faPlay}/>
+				<FontAwesomeIcon
+					onClick={playSongHandler}
+					className="play"
+					size="2x"
+					icon={isPlaying ? faPause : faPlay}
+				/>
 				<FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight}/>
 			</div>
 			<audio
@@ -59,7 +79,7 @@ const Player = ({ isPlaying, setIsPlaying, currentSong }) => {
 				onTimeUpdate={timeUpdateHandler}
 				ref={audioRef}
 				src={currentSong.audio}
-				
+
 			></audio>
 		</div>
 	)
